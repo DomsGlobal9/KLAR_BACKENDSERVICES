35db1191f3a462336b24d5d78f9d4a385c54af8b
DomsGlobal9 <domsgloballlp@gmail.com>
2026-02-27 11:44:09 +0530

    Auth controller modified

--- auth-service/src/routes/auth.routes.ts
+++ auth-service/src/routes/auth.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { signupB2B, loginB2B, logoutB2B, me } from "../controllers/auth.controller";
+import { signupB2B, loginB2B, logoutB2B, me, validateToken } from "../controllers/auth.controller";
 import { authenticateJWT } from "../middlewares/authentication.middleware";
 
 const router = Router();
 
-// B2B Auth Routes
+
 router.post("/signup", signupB2B);
 router.post("/login", loginB2B);
 router.post("/logout", logoutB2B);
 router.get("/me", authenticateJWT, me);
+router.get("/validate", authenticateJWT, validateToken);
 
 export default router;
