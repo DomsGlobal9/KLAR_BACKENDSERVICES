aad4fbedd44a3cf4a640c1d6ec039b8d949df273
DomsGlobal9 <domsgloballlp@gmail.com>
2026-04-17 13:27:59 +0530

    Modify the env config file

--- FlightsSearchService/src/config/env.ts
+++ FlightsSearchService/src/config/env.ts
@@ -59,7 +59,7 @@ export const envConfig = {
     ENDPOINTS: {
       AIR_SEARCH_ALL: "/fms/v1/air-search-all",
       FARE_RULE: "/fms/v2/farerule",
-      REVIEW: "/fms/v2/review",
+      REVIEW: "/fms/v1/review",
       SEAT: "/fms/v1/seat",
       REVALIDATE: "/fms/v2/revalidate",
       FARE_QUOTE: "/fms/v2/farequote",
