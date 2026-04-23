5a11889433584fc095d268648f73d96ac8c6cd14
DomsGlobal9 <domsgloballlp@gmail.com>
2026-04-23 17:06:31 +0530

    Modify the post into put

--- flight-service/src/routes/bookingLocal.routes.ts
+++ flight-service/src/routes/bookingLocal.routes.ts
@@ -4,6 +4,6 @@ import BookingLocalController from "../controllers/bookingLocal.controller";
 const router = Router();
 
 router.post("/init", BookingLocalController.createLocalBooking);
-router.post("/update", BookingLocalController.updateBookingDetails);
+router.put("/update", BookingLocalController.updateBookingDetails);
 
 export default router;
