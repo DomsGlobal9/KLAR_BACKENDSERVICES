f5bd3bc95e7a57ae2c8acce1f6c4af59c46846b6
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-05-18 18:03:13 +0530

    updates are added

--- flight-service/src/utils/flight-confirmatoin-pdf-generator.util.ts
+++ flight-service/src/utils/flight-confirmatoin-pdf-generator.util.ts
@@ -7,7 +7,7 @@ export const generatePdfFromHtml = async (html: string): Promise<Buffer> => {
     });
     const page = await browser.newPage();
     
-    await page.setContent(html, { waitUntil: 'networkidle0' });
+    await page.setContent(html, { waitUntil: 'load' });
     
     const pdfBuffer = await page.pdf({
         format: 'A4',
