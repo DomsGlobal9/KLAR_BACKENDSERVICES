6d22565c6456e85a5056669cb48cb30b4347d232
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-06-08 17:24:42 +0530

    updates are added

--- auth-service/src/repositories/bookingPayment.repository.ts
+++ auth-service/src/repositories/bookingPayment.repository.ts
@@ -1,11 +1,153 @@
+// import { Types } from "mongoose";
+// import { UserModel } from "../models/user.model";
+// import { Wallet } from "../models/wallet.model";
+// import { WalletTransaction } from "../models/walletTransaction.model";
+
+// export class BookingPaymentRepository {
+    
+//     static async getUserDocumentById(userId: Types.ObjectId): Promise<any> {
+//         return mongoose.connection.db?.collection("users").findOne({ _id: userId });
+//     }
+
+//     static async getWallet(userId: Types.ObjectId) {
+//         return Wallet.findOne({ userId });
+//     }
+
+//     static async checkExistingPayment(bookingId: string) {
+//         return WalletTransaction.findOne({
+//             referenceId: bookingId,
+//             referenceType: "BOOKING",
+//             direction: "DEBIT",
+//         });
+//     }
+
+//     static async deductBalance(walletId: Types.ObjectId, totalPrice: number) {
+//         const amount = totalPrice;
+//         return Wallet.findOneAndUpdate(
+//             {
+//                 _id: walletId,
+//                 balance: { $gte: amount },
+//             },
+//             {
+//                 $inc: { balance: -amount },
+//             },
+//             { new: true }
+//         );
+//     }
+
+//     static async createTransaction(data: any) {
+//         return WalletTransaction.create(data);
+//     }
+
+//     /**
+//      * Deduct balance allowing negative values
+//      */
+//     static async deductBalanceAllowNegative(
+//         walletId: Types.ObjectId,
+//         amount: number
+//     ) {
+//         const wallet = await Wallet.findByIdAndUpdate(
+//             walletId,
+//             {
+//                 $inc: { balance: -amount },
+//                 $set: { updatedAt: new Date() }
+//             },
+//             { new: true }
+//         );
+
+//         return wallet;
+//     }
+
+//     /**
+//      * Set exact balance (for negative balance scenario)
+//      */
+//     static async setBalance(
+//         walletId: Types.ObjectId,
+//         balance: number
+//     ) {
+//         const wallet = await Wallet.findByIdAndUpdate(
+//             walletId,
+//             {
+//                 $set: {
+//                     balance: balance,
+//                     updatedAt: new Date()
+//                 }
+//             },
+//             { new: true }
+//         );
+
+//         return wallet;
+//     }
+
+//     /**
+//      * Add balance to wallet (for reimbursement)
+//      */
+//     static async addBalance(
+//         walletId: Types.ObjectId,
+//         amount: number
+//     ) {
+//         const wallet = await Wallet.findByIdAndUpdate(
+//             walletId,
+//             {
+//                 $inc: { balance: amount },
+//                 $set: { updatedAt: new Date() }
+//             },
+//             { new: true }
+//         );
+
+//         return wallet;
+//     }
+
+//     /**
+//      * Get user details including createdBy
+//      */
+//     static async getUserWithParent(userId: Types.ObjectId) {
+//         return await UserModel.findById(userId)
+//             .select('_id email mobile roles createdBy businessProfile')
+//             .lean();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Types } from "mongoose";
 import { UserModel } from "../models/user.model";
 import { Wallet } from "../models/wallet.model";
 import { WalletTransaction } from "../models/walletTransaction.model";
 
 export class BookingPaymentRepository {
     
+    /**
+     * Get user details including hierarchy flags
+     */
     static async getUserDocumentById(userId: Types.ObjectId): Promise<any> {
-        return mongoose.connection.db?.collection("users").findOne({ _id: userId });
+        return UserModel.findById(userId)
+            .select('_id email mobile roles createdBy businessProfile')
+            .lean();
     }
 
     static async getWallet(userId: Types.ObjectId) {
