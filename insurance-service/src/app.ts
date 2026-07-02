a259b234d04a8f675102b380fdf40c58dadac712
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-07-02 16:34:05 +0530

    Path Parsing Validation Error is fixed

--- insurance-service/src/app.ts
+++ insurance-service/src/app.ts
@@ -20,7 +20,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('/*', cors(corsOptions));
+app.options('/*splat', cors(corsOptions));
 
 app.use(express.json({ limit: "2mb" }));
 
