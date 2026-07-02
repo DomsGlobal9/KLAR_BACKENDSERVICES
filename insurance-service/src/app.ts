bc4820186f38ee7b8aa8b0a665fa14cbcedcf22d
DomsGlobal9 <domsgloballlp@gmail.com>
2026-07-02 15:30:21 +0530

    Modifyt app.ts in insurance service

--- insurance-service/src/app.ts
+++ insurance-service/src/app.ts
@@ -20,7 +20,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json({ limit: "2mb" }));
 
@@ -34,7 +34,7 @@ app.get("/", (_req: Request, res: Response) => {
 });
 
 app.use("/api/insurance", routes);
-// app.use("/",              routes); 
+app.use("/",              routes); 
 
 app.use(errorHandler);
 
