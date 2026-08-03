5393e9119202b05429adf7a264fe5e72df6b36d7
vaishnavi9908 <vaishnavi.developer@domsglobal.co>
2026-08-03 13:03:36 +0530

    category field is added in the types

--- charter-service/src/types/charter.types.ts
+++ charter-service/src/types/charter.types.ts
@@ -1,10 +1,17 @@
 export type CharterSource = "b2b" | "b2c";
 
+export type CharterCategory =
+  | "Private Jets"
+  | "Helicopter Charter"
+  | "Corporate Charter"
+  | "Group Charter";
+
 export interface ICharterBooking {
   from: string;
   to: string;
   departureDateTime: Date;
   passengers: number;
+  category: CharterCategory;
   fullName: string;
   mobileNumber: string;
   email: string;
